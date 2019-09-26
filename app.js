09d7012b37940078c855bd4fdddfc013ecefdc8e
masunile <helge@furion.se>
2019-09-26 15:55:39 +0200

    Fixed typo in Skycons

--- app.js
+++ app.js
@@ -49,6 +49,6 @@ window.addEventListener('load', ()=> {
         const skycons = new Skycons({ color: "white"});
         const currentIcon = icon.replace(/-/g, "_").toUpperCase();
         skycons.play();
-        return skycons.set(iconID, skycons[currentIcon]);
+        return skycons.set(iconID, Skycons[currentIcon]);
     }
 });
